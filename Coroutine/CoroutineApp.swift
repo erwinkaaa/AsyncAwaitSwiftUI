445d799125ffe65fca67bbc876b1884c90d5f4ef
erwin <erwinkurniawanadi@gmail.com>
2022-07-06 16:18:45 +0700

    Initial Commit

---
+++ Coroutine/CoroutineApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoroutineApp.swift
+//  Coroutine
+//
+//  Created by ACI on 06/07/22.
+//
+
+import SwiftUI
+
+@main
+struct CoroutineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
